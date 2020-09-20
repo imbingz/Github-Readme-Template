9de6e5665e18e76c65e8202bcfdf593a717e28aa
Bing Z <bing@bings-mbp.lan>
2020-09-20 14:20:23 -0600

    added comments

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+/* GLOBAL VARIALBES ==================================================================================================== */
+
+//Import inquirer and node modules 
 const inquirer = require('inquirer');
+const fs = require('fs')
+const util = require('util')
 
-let date = new Date().getFullYear();
+// Get the current year 
+let year = new Date().getFullYear();
 
-console.log(date);
 
+// Set inquirer prompt questions
 console.log('Welcome to Github README generator');
 
 const questions = [
-	{
+  {
+    //Author Name
 		type: 'input',
 		message: 'Enter the full name(s) of the project author(s)',
 		name: 'authorName',
@@ -20,7 +27,8 @@ const questions = [
 			return 'Please enter the FULL name(s) of the project author(s)';
 		},
 	},
-	{
+  {
+    //Github Username
 		type: 'input',
 		message: 'Enter your github username',
 		name: 'username',
@@ -32,7 +40,8 @@ const questions = [
 			return 'Enter your github username';
 		},
 	},
-	{
+  {
+    // Github REPO name
 		type: 'input',
 		message: 'Enter your Github Project Repo name',
 		name: 'repoName',
@@ -44,13 +53,15 @@ const questions = [
 			return 'Enter your Github Project Repo name';
 		},
 	},
-	{
+  {
+    //Check on title 
 		type: 'confirm',
 		message: 'Do you want to use your Repo Name as your project title?',
 		name: 'useRepoName',
 		default: true,
 	},
-	{
+  {
+    // Enter Title if different from REPO name
 		type: 'input',
 		message: 'Enter your project title',
 		name: 'title',
@@ -65,13 +76,15 @@ const questions = [
 			return 'Enter your project title';
 		},
 	},
-	{
+  {
+    //Check on Screenshots and/or Demo
 		type: 'confirm',
 		name: 'screenshots',
 		message: 'Would you like to add screenshots or demo to README?',
 		default: false,
 	},
-	{
+  {
+    // Get image path and/or URL
 		type: 'input',
 		name: 'imageURL',
 		message: 'Enter the image paths or urls of screenshots or demo',
@@ -86,7 +99,8 @@ const questions = [
 			return 'Enter the image paths or urls of screenshots or demo';
 		},
 	},
-	{
+  {
+    //Project objective
 		type: 'input',
 		message: 'What is your project objective?',
 		name: 'objective',
@@ -98,7 +112,8 @@ const questions = [
 			return 'What is your project objective?';
 		},
 	},
-	{
+  {
+    // Project user-story
 		type: 'input',
 		message: 'Provide the User Story for your propject',
 		name: 'userStory',
@@ -110,7 +125,8 @@ const questions = [
 			return 'Provide the User Story for your propject';
 		},
 	},
-	{
+  {
+    //Technologies used
 		type: 'input',
 		message: 'List the technologies used for the Project',
 		name: 'technologies',
@@ -122,7 +138,8 @@ const questions = [
 			return 'List the technologies used for the Project';
 		},
 	},
-	{
+  {
+    //Installation
 		type: 'input',
 		message: 'What are the steps required to install your project?',
 		name: 'installation',
@@ -134,7 +151,8 @@ const questions = [
 			return 'What are the steps required to install your project?';
 		},
 	},
-	{
+  {
+    // How to use
 		type: 'input',
 		message: 'Provide instructions on how to use your project',
 		name: 'usage',
@@ -146,17 +164,20 @@ const questions = [
 			return 'Provide instructions on how to use your project';
 		},
 	},
-	{
+  {
+    //Credits / Reference (optional)
 		type: 'input',
 		message: 'Enter the parties and links you like to give credits to',
 		name: 'credits',
 	},
-	{
+  {
+    // Tests (optional)
 		type: 'input',
 		message: 'How do you test your project?',
 		name: 'test',
 	},
-	{
+  {
+    // License 
 		type: 'list',
 		message: 'Select license',
 		name: 'license',
@@ -165,6 +186,12 @@ const questions = [
 	},
 ];
 
+
+/* FUNCTIONS ==================================================================================================== */
+
+
 inquirer.prompt(questions).then((answers) => {
 	console.log(JSON.stringify(answers, null, '\t'));
 });
+
+
