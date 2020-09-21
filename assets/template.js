21292e76fd062104c4c77a51a5bb53eb5ae07ab4
imbinz <bingz@me.com>
2020-09-20 19:42:14 -0600

    corrected badge link and license value

--- assets/template.js
+++ assets/template.js
@@ -4,24 +4,15 @@ let year = new Date().getFullYear();
 // Create a readme Template
 
 function generateREADME(answers) {
-return ` 
+	return ` 
   # ${answers.title}
 
   <p>
   <a href="https://github.com/${answers.username}" target="_blank">
     <img src="https://img.shields.io/github/followers/${answers.username}?label=Follow&logoColor=purple&style=social" alt="github-follow">
   </a>
-  <a href="https://choosealicense.com/licenses/mit/" target="_blank">
-    <img alt="license-mit" src="https://img.shields.io/badge/License-MIT-brightgreen.svg" />
-  </a>
-  <a href="https://choosealicense.com/licenses/mpl-2.0/" target="_blank">
-    <img alt="license-mozilla" src="https://img.shields.io/badge/License-Mozilla%20PL%202.0-blue.svg" />
-  </a>
-  <a href="https://choosealicense.com/licenses/apache-2.0/"  target="_blank">
-    <img alt="license-apache" src="https://img.shields.io/badge/License-Apache%202.0-brightgreen.svg" />
-  </a>
-  <a href="https://choosealicense.com/licenses/gpl-3.0/"  target="_blank">
-    <img alt="license-gnu" src="https://img.shields.io/badge/License-GNU%20GPLv3-success.svg" />
+  <a href="https://choosealicense.com/licenses/${answers.license}/" target="_blank">
+    <img alt="license-mit" src="https://img.shields.io/badge/License-${answers.license.toUpperCase()}-brightgreen.svg" />
   </a>
   <a href="https://nodejs.org/en/" target="_blank">
     <img alt="node.js" src="https://img.shields.io/node/v/c?color=blueviolet" />
@@ -29,8 +20,8 @@ return `
   <a href="https://www.npmjs.com/package/inquirer" target="_blank">
     <img alt="npm" src="https://img.shields.io/npm/v/npm?color=important&logo=npm" />
   </a>
-  <a href="https://twitter.com/imbingz" target="_blank">
-    <img alt="twitter-follow" src="https://img.shields.io/twitter/follow/imbingz?label=Follow&style=social" />
+  <a href="https://twitter.com/${answers.twitterName}" target="_blank">
+    <img alt="twitter-follow" src="https://img.shields.io/twitter/follow/${answers.twitterName}?label=Follow&style=social" />
   </a>
 </p>
 
@@ -78,15 +69,14 @@ return `
 
   ## Author Info
   Contact the author with any questions! < /br>
-  Github Username: ${answers.username}
+   Github link: [${answers.username}](https://github.com/${answers.username})
 
   ## License
-  This project is [${answers.name}](https://choosealicense.com/licenses/${answers.value}/) licensed.<br />
+  This project is [${answers.license.toUpperCase()}](https://choosealicense.com/licenses/${answers.license}/) licensed.<br />
 
   Copyright © ${year} [${answers.authorName}]
 
   `;
-
 }
 
-module.exports = generateREADME; 
+module.exports = generateREADME;
